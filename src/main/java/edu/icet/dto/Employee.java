b10d3bd3f08b16af14c398157fa0f95b7000e677
poorviUvindhya <poorviwejeweera@gmail.com>
2025-04-29 10:23:17 +0530

    dto and entity

---
+++ src/main/java/edu/icet/dto/Employee.java
@@ -0,0 +1,16 @@
+package edu.icet.dto;
+
+import lombok.*;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee {
+    private Integer id;
+    private String name;
+    private String email;
+    private LocalDate craetedAt;
+    private LocalDate updateAt;
+}
---
+++ src/main/java/edu/icet/entity/EmployeeEntity.java
@@ -0,0 +1,25 @@
+package edu.icet.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Employee")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private LocalDate craetedAt;
+    private LocalDate updateAt;
+}
